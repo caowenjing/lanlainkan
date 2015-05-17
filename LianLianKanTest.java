040555761f052b74f00997b06104b160dbfcaef9
caowenjing <806541288@qq.com>
2015-05-17 12:40:29 +0800

    Create LianLianKanTest.java

---
+++ LianLianKanTest.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LianLianKanTest {
+
+	@Test
+	public void testMain() {
+		
+			 Double expectedAnswer = Double.valueOf(12);
+			    Double actualAnswer = Double.valueOf(2*6);
+			    assertEquals(expectedAnswer, actualAnswer);
+
+	}
+
+}
